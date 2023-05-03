9d7e3cff9d39638a81a0c34bebc268fce91bfe7e
Michael <mihay0113@gmail.com>
2023-05-03 18:07:01 +0300

    created app

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {
+  Box,
+  Button,
+  Modal,
+  Stack,
+  TextField,
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { RootState } from './app/store';
+import { useState } from 'react';
+import CityCard from './components/CityCard';
+import CityDetail from './components/CityDetail';
+import ErrorModal from './components/ErrorModal';
+import { City } from './types';
+import { useAppDispatch } from './app/hooks';
+import * as citiesActions from './features/cities';
+import { fetchCities } from './services/api';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  const cities = useSelector((state: RootState) => state.cities);
+  const [selectedCity, setSelectedCity] = useState<City | null>(null);
+  const [newCityName, setNewCityName] = useState('');
+
+  const [isError, setIsError] = useState(false);
+
+  const style = {
+    position: 'absolute' as const,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    minWidth: '30%',
+    width: 'min-content',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  async function fetchCity(newCityName: string) {
+    const response = await fetchCities(newCityName);
+
+    if (response.ok) {
+      const data = await response.json();
+      dispatch(citiesActions.add(data));
+      return data;
+    } else {
+      setIsError(true);
+    }
+  }
+
+  const handleAddCity = async (event: React.FormEvent) => {
+    event.preventDefault();
+    fetchCity(newCityName);
+    setNewCityName('');
+  };
+
+  const handleCityCardClick = (city: City) => {
+    setSelectedCity(city);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedCity(null);
+    setIsError(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Box sx={{ margin: '0 24px' }}>
+      <form onSubmit={handleAddCity}>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '24px',
+          }}
+        >
+          <TextField
+            label="City Name"
+            required
+            value={newCityName}
+            onChange={(e) => setNewCityName(e.target.value)}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Add
+          </Button>
+        </Stack>
+      </form>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '24px' }}>
+        {Object.values(cities.cities).map((city) => (
+          <Box key={city.id} onClick={() => handleCityCardClick(city)}>
+            <CityCard city={city} />
+          </Box>
+        ))}
+      </Box>
+      <Modal open={selectedCity !== null || isError} onClose={handleCloseModal}>
+        <Box sx={style}>
+          {selectedCity !== null && <CityDetail city={selectedCity} />}
+          {isError && <ErrorModal />}
+        </Box>
+      </Modal>
+    </Box>
   );
 }
 
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import cities from '../features/cities';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    cities: cities,
+  }
 });
 
+export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export default store;
